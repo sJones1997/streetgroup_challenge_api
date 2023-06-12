d33989eb2534ba085c0c86dd96f006e206c9ec80
sJones1997 <sean.g.jones97@gmail.com>
2023-06-12 11:41:08 +0100

    finished backend

--- app/Services/HomeownerService.php
+++ app/Services/HomeownerService.php
@@ -36,63 +36,105 @@ class HomeownerService {
 
     private function formatExtractedData($extractedHomeownerData): array {
 
-        $formattedData = array();
+        $formattedHomeowners = array();
+
+        //Remove the title
+        array_shift($extractedHomeownerData);
 
         foreach($extractedHomeownerData as $homeownerRecord){
+
+            //Check to see if there are multiple homeowners in a single record
             if($this->hasMultipleRecords($homeownerRecord)){
-                $formattedData = $this->separateRecords($homeownerRecord, $formattedData);
+                $formattedHomeowners = $this->separateRecords($homeownerRecord, $formattedHomeowners);
             } else {
-                $formattedData[] = $homeownerRecord;
-            }
+                $formattedHomeowners[] = $this->splitinToDesiredFormat($homeownerRecord);
+            }   
         }
 
-        return $formattedData;
+        return $formattedHomeowners;
     }
-
 
     private function hasMultipleRecords($homeownerRecord): bool {
         return count(explode(" ", $homeownerRecord)) > 3;
     }
 
-    private function separateRecords($homeownerRecord, $formattedData): array {
+    private function separateRecords($homeownerRecord, $formattedHomeowners): array {
 
         $explodedRecord = null;
         $splitOnAnd = explode(self::AND_SPLIT , $homeownerRecord);
         $splitOnAmpersand = explode(self::AMPERSAND_SPLIT, $homeownerRecord);
 
+        //If there are multiple, split on conjunction and use larger array
         if(count($splitOnAmpersand) > count($splitOnAnd)){
             $explodedRecord = $splitOnAmpersand;
         } else {
             $explodedRecord = $splitOnAnd;
         }
 
-        return $this->includeRequiredFields($explodedRecord, $formattedData);
+        return $this->includeRequiredFields($explodedRecord, $formattedHomeowners);
 
     }
 
-    private function includeRequiredFields($splitRecord, $formattedData): array {
+    private function includeRequiredFields($splitRecord, $formattedHomeowners): array {
 
-        $recordOne = explode(' ', trim($splitRecord[0]));
-        $recordTwo = explode(' ', trim($splitRecord[1]));
+        //After split, missing required field (last name) could be missing from one record so do a check on both
 
-        $recordOne = $this->addMissingLastName($recordOne, $recordTwo);
-        $recordTwo = $this->addMissingLastName($recordTwo, $recordOne);
+        $recordOne = $this->addMissingLastName($splitRecord[0], $splitRecord[1]);
+        $recordTwo = $this->addMissingLastName($splitRecord[1], $splitRecord[0]);
 
-        $formattedData[] = implode(" ", $recordOne);
-        $formattedData[] = implode(" ", $recordTwo);
-
-        return $formattedData;
+        return array_merge($formattedHomeowners, [
+            $this->splitinToDesiredFormat($recordOne), 
+            $this->splitinToDesiredFormat($recordTwo)
+            ]
+        );
         
     }
 
 
     private function addMissingLastName($recordOne, $recordTwo){
+        
+        $recordOne = explode(' ', trim($recordOne));
+        $recordTwo = explode(' ', trim($recordTwo));
+
         if(count($recordOne) == 1){
             $recordOne[] = $recordTwo[array_key_last($recordTwo)];
         }
-        return $recordOne;
+
+        return implode(" ", $recordOne);
+    }    
+
+    private function splitinToDesiredFormat($homeownerRecord): array{
+
+        //Once all records have been correctly formatted, split the record into the desired format given in the brief
+        $homeownerRecordSplit = explode(" ", $homeownerRecord);
+
+        if(count($homeownerRecordSplit) > 1){
+
+            $person = array();
+            $person["title"] = $homeownerRecordSplit[0];
+
+            if(count($homeownerRecordSplit) > 2) {
+                if(!$this->isInitial($homeownerRecordSplit[1])){
+                    $person['firstName'] = $homeownerRecordSplit[1];
+                } else {
+                    $person['initial'] = $homeownerRecordSplit[1];
+                }
+            }
+
+            $person["lastName"] = $homeownerRecordSplit[array_key_last($homeownerRecordSplit)];
+            return $person;
+        }
+
+        return array();
     }
 
-    
+
+    private function isInitial($initial){
+        $regex = '/[A-Za-z]\./';
+        if(preg_match($regex, $initial) || strlen($initial) == 1){
+            return true;
+        }
+        return false;
+    }
 
 }
--- tests/Unit/HomeownerServiceTest.php
+++ tests/Unit/HomeownerServiceTest.php
@@ -18,9 +18,27 @@ class HomeownerServiceTest extends TestCase {
     const CSV_DATA = [
         'homeowner',
         "Mr S Jones",
+        "Mr S. Jones",
         "Mr and Mrs Doe",
         "Mr Sean Jones & Mrs Jones"
     ];
+
+    const CSV_DATA_INITIALS = [
+        'homeowner',
+        "Mr S Jones",
+        "Mr S. Jones"
+    ];
+
+    const CSV_DATA_MULTIPLE = [
+        'homeowner',
+        "Mr and Mrs Doe"
+    ];
+
+    const CSV_DATA_SPLIT_FULL = [
+        'homeowner',
+        "Mr Sean Jones & Mrs F. Jones"
+    ];
+
 
     
     public function testValidateData(){
@@ -36,8 +54,87 @@ class HomeownerServiceTest extends TestCase {
         $formattedData = $this->homeownerService->validateData($file);
 
         //Then
-        $this->assertEquals(count($formattedData), 6);
+        $this->assertEquals(6, count($formattedData));
     }
+
+    public function testInitials(){
+
+        $this->homeownerService = new HomeownerService();
+
+        //Given
+        $file = $this->createMockCsv(self::CSV_DATA_INITIALS, self::VALID_FILE);
+
+        Log::info(filetype($file));
+
+        //When
+        $formattedData = $this->homeownerService->validateData($file);
+
+        $recordOne = $formattedData[0];
+        $recordTwo = $formattedData[1];
+
+        Log::info(var_dump($formattedData, true));
+
+        $this->assertEquals("Mr", $recordOne["title"]);
+        $this->assertEquals("S", $recordOne["initial"]);
+        $this->assertEquals("Jones", $recordOne["lastName"]);
+
+        $this->assertEquals("Mr", $recordTwo["title"]);
+        $this->assertEquals("S.", $recordTwo["initial"]);
+        $this->assertEquals("Jones", $recordTwo["lastName"]);
+
+    }
+
+
+    
+    public function testLastNameCarryOver(){
+
+        $this->homeownerService = new HomeownerService();
+
+        //Given
+        $file = $this->createMockCsv(self::CSV_DATA_MULTIPLE, self::VALID_FILE);
+
+        Log::info(filetype($file));
+
+        //When
+        $formattedData = $this->homeownerService->validateData($file);
+
+        $recordOne = $formattedData[0];
+        $recordTwo = $formattedData[1];
+
+        $this->assertEquals("Mr", $recordOne["title"]);
+        $this->assertEquals("Doe", $recordOne["lastName"]);
+
+        $this->assertEquals("Mrs", $recordTwo["title"]);
+        $this->assertEquals("Doe", $recordTwo["lastName"]);
+
+    }
+
+
+    public function testSplitTwoFullRecords(){
+
+        $this->homeownerService = new HomeownerService();
+
+        //Given
+        $file = $this->createMockCsv(self::CSV_DATA_SPLIT_FULL, self::VALID_FILE);
+
+        Log::info(filetype($file));
+
+        //When
+        $formattedData = $this->homeownerService->validateData($file);
+
+        $recordOne = $formattedData[0];
+        $recordTwo = $formattedData[1];
+
+        $this->assertEquals("Mr", $recordOne["title"]);
+        $this->assertEquals("Sean", $recordOne["firstName"]);
+        $this->assertEquals("Jones", $recordOne["lastName"]);
+
+        $this->assertEquals("Mrs", $recordTwo["title"]);
+        $this->assertEquals("F.", $recordTwo["initial"]);
+        $this->assertEquals("Jones", $recordTwo["lastName"]);
+
+    }
+
 
 
     public function testInvalidFileFormat() {
@@ -46,8 +143,6 @@ class HomeownerServiceTest extends TestCase {
 
         //Given
         $file = $this->createWrongFileFormat(self::INVALID_FILE);
-
-        Log::info(filetype($file));
 
         //When
         $formattedData = $this->homeownerService->validateData($file);
